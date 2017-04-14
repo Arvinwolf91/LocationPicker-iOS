87b14ae716c50b59f042ec0bf64e0ba62e78aeff
Arvin Sanmuga <arvin.sanmuga@hexainitiative.com>
2017-04-14 11:45:23 +0800

    [] Do not set initial location

--- ArvinLocationPicker/LocationEngine.swift
+++ ArvinLocationPicker/LocationEngine.swift
@@ -19,24 +19,24 @@ class LocationEngine: NSObject {
         let locationPicker = LocationPickerViewController()
         
         // you can optionally set initial location
-        let location = CLLocation(latitude: 35, longitude: 35)
-        let initialPlacemark = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 10, longitude: 20), addressDictionary: ["City": "Palo Alto", CNPostalAddressStreetKey as String: "CA"])
-        let initialLocation = Location(name: "Home", location: location, placemark:initialPlacemark)
-        locationPicker.location = initialLocation
+//        let location = CLLocation(latitude: 35, longitude: 35)
+//        let initialPlacemark = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 10, longitude: 20), addressDictionary: ["City": "Palo Alto", CNPostalAddressStreetKey as String: "CA"])
+//        let initialLocation = Location(name: "Home", location: location, placemark:initialPlacemark)
+//        locationPicker.location = initialLocation
         // button placed on right bottom corner
         locationPicker.showCurrentLocationButton = true // default: true
         // default: navigation bar's `barTintColor` or `.whiteColor()`
         locationPicker.currentLocationButtonBackground = .blue
-        
         // ignored if initial location is given, shows that location instead
         locationPicker.showCurrentLocationInitially = true // default: true
-        
         locationPicker.mapType = .standard // default: .Hybrid
+        
         // for searching, see `MKLocalSearchRequest`'s `region` property
         locationPicker.useCurrentLocationAsHint = true // default: false
         locationPicker.searchBarStyle = .prominent
         locationPicker.searchBarPlaceholder = "Search places" // default: "Search or enter an address"
         locationPicker.searchHistoryLabel = "Previously searched" // default: "Search History"
+        
         locationPicker.resultRegionDistance = 500 // default: 600
         
         // Selected location
